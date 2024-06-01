813628aa2cfa17bb8faa709fd9d2ea1291e5da6f
adiazwaterman <alexisdiaz-waterman2024@u.northwestern.edu>
2024-06-01 15:50:19 -0500

    adding update operation

--- recipe_step_tracker_api/schema.py
+++ recipe_step_tracker_api/schema.py
@@ -2,3 +2,21 @@ import strawberry
 import strawberry_django
 from strawberry_django.optimizer import DjangoOptimizerExtension
 from strawberry_django import mutations
+
+from .types import RecipeStep, RecipeStepInput, UpdateRecipeStep
+
+@strawberry.type
+class Query:
+    recipe_steps: list[RecipeStep] = strawberry_django.field()
+
+@strawberry.type
+class Mutation:
+    add_recipe_step: RecipeStep = mutations.create(RecipeStepInput)
+    update_recipe_step: RecipeStep = mutations.update(UpdateRecipeStep)
+
+
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+    extensions=[DjangoOptimizerExtension]
+    )
--- recipe_step_tracker_api/types.py
+++ recipe_step_tracker_api/types.py
@@ -2,3 +2,21 @@ import strawberry_django
 from strawberry import auto, ID
 
 from .models import RecipeStep as RecipeStepModel
+
+@strawberry_django.type(RecipeStepModel)
+class RecipeStep:
+    id: ID
+    step: auto
+    completed: auto
+
+@strawberry_django.input(RecipeStepModel)
+class RecipeStepInput:
+    step: auto
+    completed: auto
+
+@strawberry_django.partial(RecipeStepModel)
+class UpdateRecipeStep:
+    id: auto
+    step: auto
+    completed: auto
+ 
